b9f014feb70ce511f5cee2ee602deae0280c5ba2
Jacob Cook <j.h.cook22@gmail.com>
2020-10-13 14:31:05 -0500

    Update ClassProject.py
    final project

--- ClassProject.py
+++ ClassProject.py
@@ -5,56 +5,66 @@ Written: 10/13/2020
 '''
 import requests
 import json
+loopAgain = "yes"
 APIID = "c98593e412245c9ce904a86281016ff1"
-validLoc = False
+
 def inputData():
     print("Please enter a zip code or city name: ")
     return input()
 
-def determineType(l):
-    if l.isalpha:
-        locType = "city"
-    elif l.isnumeric:
-        locType = "zip"
+def determineType(l): #use a try catch to check if input is zip or City
+    try:
+        l = int(l)      
+        return "zip"
+    except ValueError:
+        return "city"
+
+def cityAPICall(l):     #copied partly from https://knasmueller.net/using-the-open-weather-map-api-with-python
+    url = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=imperial" % (l,APIID)
+    print("Connecting to Open Weather API...")
+    response = requests.get(url).json()
+    if response["cod"] == 200:
+        print("Connection Successful!")
+        return response
     else:
-        locType = "error"
-
-    return locType 
-
-def cityAPICall(l):     #copied from https://knasmueller.net/using-the-open-weather-map-api-with-python
-    url = "api.openweathermap.org/data/2.5/weather?q=%s&appid=%s" % (l,APIID)
-    response = requests.get(url)
-    return json.loads(response.text)
+        return "error"    
 
 def zipAPICall(l):      #copied from https://knasmueller.net/using-the-open-weather-map-api-with-python
-    url = "api.openweathermap.org/data/2.5/weather?q=%s&appid=%s" % (l,APIID)
-    response = requests.get(url)
-    return json.loads(response.text)
+    url = "https://api.openweathermap.org/data/2.5/weather?zip=%s&appid=%s&units=imperial" % (l,APIID)
+    print("Connecting to Open Weather API...")
+    response = requests.get(url).json()
+    if response["cod"] == 200:
+        print("Connection Successful!")
+        return response
+    else:
+        return "error"    
 
 def formatAndDisplay(rawData):
-    print(rawData)
+    print("City:", rawData["name"])
+    print("Temp:", rawData["main"]["temp"], "F°")
+    print("Feels Like: ", rawData["main"]["feels_like"], "F°")
+    print("Air Pressure: ", rawData["main"]["pressure"], "hPa")
+    print("Humidity:", rawData["main"]["humidity"], "%")
+    print("Wind Speed:", rawData["wind"]["speed"])
+
 
 def main():
-    location = inputData()
-    locType = determineType(location)
-    if locType == "city":
-        rawData = cityAPICall(location)
-    elif locType == "zip":
-        rawData = zipAPICall(location)
-    else:
-        validLoc = False
-        print("Invalid location. You entered:", location, ". Please Try again.")
+    loopAgain = "yes"
+    while loopAgain == "yes":
+        location = inputData()
+        locType = determineType(location)
+        if locType == "city":
+            rawData = cityAPICall(location)
+        else:
+            rawData = zipAPICall(location)
 
-    if validLoc == True: 
-        formatAndDisplay(rawData)
-    else:
-        print("Location not found. Please try again.")
+        if rawData == "error": 
+            print("Error, invalid input.")
+        else:
+            formatAndDisplay(rawData)
+        print("Would you like to enter another? [yes or no]")
+        loopAgain = input()
+    print("Thanks for using Jacob's Weather App! Brought to you by OpenWeatherMap")
+    print("Learn more about this API at: https://openweathermap.org/")
 
 main()
-    
-    
-    
-
-
-
-
